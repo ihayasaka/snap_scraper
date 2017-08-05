e724703b32925af694644307553efba282f1cd95
ihayasaka <sirouma.09@gmail.com>
2017-08-05 13:53:43 +0900

    Added code to move to Harajuku page

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome(executable_path='/Users/ikuma/Downloads/chromedriver')
-
 browser.get('http://www.style-arena.jp/')
 assert "東京のストリートファッション最新情報 | スタイルアリーナ" in browser.title
 
-elements = browser.find_elements_by_class_name('cms_list_content')
-for elem in elements:
-    print(elem)
+elem = browser.find_element_by_xpath("//*[@id='top_area_wrap']/div/div/ul/li[1]/div/div/h2/a")
+print(elem.get_attribute('href'))
+browser.get(elem.get_attribute('href'))
 
 browser.quit()
