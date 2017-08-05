773e3b4d2346099c4b564b031f3658de26dd68d8
ihayasaka <sirouma.09@gmail.com>
2017-08-05 14:42:23 +0900

    Added logger lib and fixed errors from flake8 and pylint

--- lib/pom/Home.py
+++ lib/pom/Home.py
@@ -1,9 +1,13 @@
+from lib.util.Logger import get_module_logger
+
+
 class Home:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = get_module_logger(__name__)
 
     def get_harajuku_url(self):
         elem = self.driver.find_element_by_xpath("//*[@id='top_area_wrap']/div/div/ul/li[1]/div/div/h2/a")
-        print(elem.get_attribute('href'))
+        self.logger.debug(elem.get_attribute('href'))
         return elem.get_attribute('href')
